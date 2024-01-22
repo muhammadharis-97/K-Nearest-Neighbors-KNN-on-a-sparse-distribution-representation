a7b052e1ca3d2ad9b116693cddfb80f2d60aa9d2
Muhammad Haris <mohammadharis882@gmail.com>
2024-01-22 09:42:25 +0100

    Demo

---
+++ source/NeoCortexApi/Classifiers/KNNClassifier_GlobalVariable/KNNClassifier_GlobalVariable/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace KNNClassifier_GlobalVariable
+{
+    public class Knnprogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin with KNN Classification")
+            double[][] trainData = Loaddata();
+        
+        
+        
+        
+        }
+
+        static double[][] Loaddata()
+        {
+            double[][] data = new double[5][];
+            data[0] = new double [] { 2.0, 4.0, 1};
+            data[1] = new double[] { 3.0, 4.0, 1 };
+            data[2] = new double[] { 4.0, 2.0, 1 };
+            data[3] = new double[] { 5.0, 3.0, 1 };
+            data[4] = new double[] { 1.0, 4.0, 1 };
+            return data;
+        }
+    }
+}
