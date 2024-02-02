20363c56e08e6ab27f12afa9418c22f7f1b690ff
Muhammad Haris <mohammadharis882@gmail.com>
2024-02-02 11:37:59 +0100

    KNN

---
+++ source/KNN Implementation/KNN-Task1/KNNTask1/KNN/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN
+{
+    public class KNN_GlobalVariable
+    {
+
+        static int Classifier(double[] unknown, double[][] trainData, int numClasses, int k)
+        {
+            int n = trainData.Length;
+            IndexAndDistance[] info = new IndexAndDistance[n];
+            for (int i = 0; i < n; i++)
+            {
+                IndexAndDistance curr = new IndexAndDistance();
+                double dist = Distance(unknown, trainData[i]);
+                curr.idx = i;
+                curr.dist = dist;
+                info[i] = curr;
+
+            }
+            Array.Sort(info);  // sort by distance
+            int result = Vote(info, trainData, numClasses, k);
+            return result;
+
+        }
+
+
+        static double Distance(double[] unknown, double[] data)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < unknown.Length; ++i)
+                sum += (unknown[i] - data[i]) * (unknown[i] - data[i]);
+            return Math.Sqrt(sum);
+        }
+
+        static int Vote(IndexAndDistance[] info, double[][] trainData, int numClasses, int k)
+        {
+            int[] votes = new int[numClasses];  // One cell per class
+            for (int i = 0; i < k; ++i)
+            {       // Just first k
+                int idx = info[i].idx;            // Which train item
+                int c = (int)trainData[idx][2];   // Class in last cell
+                ++votes[c];
+            }
+            int mostVotes = 0;
+            int classWithMostVotes = 0;
+            for (int j = 0; j < numClasses; ++j)
+            {
+                if (votes[j] > mostVotes)
+                {
+                    mostVotes = votes[j];
+                    classWithMostVotes = j;
+                }
+            }
+            return classWithMostVotes;
+        }
+
+
+    }
+    public class IndexAndDistance : IComparable<IndexAndDistance>
+    {
+        public int idx;  // index of a training item
+        public double dist;  // distance to unknown
+        public int CompareTo(IndexAndDistance other)
+        {
+            if (this.dist < other.dist) return -1;
+            else if (this.dist > other.dist) return +1;
+            else return 0;
+        }
+
+    }
+
+}
+
--- source/KNN Implementation/KNN-Task1/KNNTask1/KNNclassifier/Program.cs
+++ source/KNN Implementation/KNN-Task1/KNNTask1/KNNclassifier/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.Write("KNN Clasifier");
+﻿using System;
+using KNN;
+
+
+/// Load Data
+
+
+Console.WriteLine("Begin with KNN Classification");
+double[][] trainData = Loaddata();
+
+//// Initializing num of features and classes
+int numFeatures = 2;
+int numClasses = 3;
+double[] unknown = new double[] { 2.01, 4.5 };
+Console.WriteLine("Predictor values: 5.25 3.75 ");
+
+/// Applying classifier for K=1
+int k = 1;
+Console.WriteLine("With k = 1");
+int predicted = Classifier(unknown, trainData, numClasses, k);
+Console.WriteLine("Predicted class = " + predicted);
+
+/// Applying classifier for K=1
+k = 4;
+Console.WriteLine("With k = 4");
+predicted = Classifier(unknown, trainData, numClasses, k);
+Console.WriteLine("Predicted class = " + predicted);
+Console.WriteLine("End kNN ");
+Console.ReadLine();
+
+
+
+
+Console.Write("KNN Clasifier");
