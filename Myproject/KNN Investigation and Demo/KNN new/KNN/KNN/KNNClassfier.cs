a24c0596420b1db0297b183c51e88b4bd6580a80
Muhammad Haris <mohammadharis882@gmail.com>
2024-03-24 02:13:54 +0100

    update

---
+++ Myproject/KNN Investigation and Demo/KNN new/KNN/KNN/KNNClassfier.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN
+{
+    // Define classes to represent the JSON structure
+    public class SequenceDataEntry
+    {
+        public Dictionary<string, List<double>> SequenceData { get; set; }
+    }
+
+    public class Dataset
+    {
+        public List<SequenceDataEntry> Data { get; set; }
+    }
+
+    public class KNNClassifier
+    {
+        public double CalculateEuclideanDistance(List<double> testData, List<double> trainData)
+        {
+            if (testData == null || trainData == null)
+                throw new ArgumentNullException("Both testData and trainData must not be null.");
+
+            if (testData.Count != trainData.Count)
+                throw new ArgumentException("testData and trainData must have the same length.");
+
+            double sumOfSquaredDifferences = 0.0;
+
+            for (int i = 0; i < testData.Count; ++i)
+            {
+                double difference = testData[i] - trainData[i];
+                sumOfSquaredDifferences += difference * difference;
+            }
+
+            return Math.Sqrt(sumOfSquaredDifferences);
+        }
+
+        public int Vote(IndexAndDistance[] info, List<int> trainLabels, int numofclass, int k)
+        {
+            int[] votes = new int[numofclass];
+
+            for (int i = 0; i < k; ++i)
+            {
+                int idx = info[i].idx;
+                int c = trainLabels[idx];
+                ++votes[c];
+            }
+
+            int classWithMostVotes = Array.IndexOf(votes, Max(votes));
+            return classWithMostVotes;
+        }
+
+        private int Max(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        public List<int> Test(List<List<double>> testingFeatures, List<List<double>> trainingFeatures, List<int> trainingLabels, int k)
+        {
+            List<int> predictedLabels = new List<int>();
+
+            foreach (var testData in testingFeatures)
+            {
+                IndexAndDistance[] info = new IndexAndDistance[trainingFeatures.Count];
+
+                for (int i = 0; i < trainingFeatures.Count; i++)
+                {
+                    double dist = CalculateEuclideanDistance(testData, trainingFeatures[i]);
+                    info[i] = new IndexAndDistance { idx = i, dist = dist };
+                }
+
+                Array.Sort(info);
+
+                int result = Vote(info, trainingLabels, 3, k);
+                predictedLabels.Add(result);
+            }
+
+            return predictedLabels;
+        }
+
+        public double CalculateAccuracy(List<int> predictedLabels, List<int> actualLabels)
+        {
+            int correctPredictions = 0;
+            for (int i = 0; i < predictedLabels.Count; i++)
+            {
+                if (predictedLabels[i] == actualLabels[i])
+                    correctPredictions++;
+            }
+            double accuracy = (double)correctPredictions / predictedLabels.Count * 100;
+            return accuracy;
+        }
+    }
+
+    public class IndexAndDistance : IComparable<IndexAndDistance>
+    {
+        public int idx;
+        public double dist;
+
+        public int CompareTo(IndexAndDistance other)
+        {
+            return dist.CompareTo(other.dist);
+        }
+    }
+}
--- Myproject/KNN Investigation and Demo/KNN new/KNN/KNN/Program.cs
+++ Myproject/KNN Investigation and Demo/KNN new/KNN/KNN/Program.cs
@@ -2,114 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using KNN;
 
 namespace KNNImplementation
 {
-    // Define classes to represent the JSON structure
-    public class SequenceDataEntry
-    {
-        public Dictionary<string, List<double>> SequenceData { get; set; }
-    }
-
-    public class Dataset
-    {
-        public List<SequenceDataEntry> Data { get; set; }
-    }
-
-    public class KNNClassifier
-    {
-        public double CalculateEuclideanDistance(List<double> testData, List<double> trainData)
-        {
-            if (testData == null || trainData == null)
-                throw new ArgumentNullException("Both testData and trainData must not be null.");
-
-            if (testData.Count != trainData.Count)
-                throw new ArgumentException("testData and trainData must have the same length.");
-
-            double sumOfSquaredDifferences = 0.0;
-
-            for (int i = 0; i < testData.Count; ++i)
-            {
-                double difference = testData[i] - trainData[i];
-                sumOfSquaredDifferences += difference * difference;
-            }
-
-            return Math.Sqrt(sumOfSquaredDifferences);
-        }
-
-        public int Vote(IndexAndDistance[] info, List<int> trainLabels, int numofclass, int k)
-        {
-            int[] votes = new int[numofclass];
-
-            for (int i = 0; i < k; ++i)
-            {
-                int idx = info[i].idx;
-                int c = trainLabels[idx];
-                ++votes[c];
-            }
-
-            int classWithMostVotes = Array.IndexOf(votes, Max(votes));
-            return classWithMostVotes;
-        }
-
-        private int Max(int[] arr)
-        {
-            int max = arr[0];
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] > max)
-                    max = arr[i];
-            }
-            return max;
-        }
-
-        public List<int> Test(List<List<double>> testingFeatures, List<List<double>> trainingFeatures, List<int> trainingLabels, int k)
-        {
-            List<int> predictedLabels = new List<int>();
-
-            foreach (var testData in testingFeatures)
-            {
-                IndexAndDistance[] info = new IndexAndDistance[trainingFeatures.Count];
-
-                for (int i = 0; i < trainingFeatures.Count; i++)
-                {
-                    double dist = CalculateEuclideanDistance(testData, trainingFeatures[i]);
-                    info[i] = new IndexAndDistance { idx = i, dist = dist };
-                }
-
-                Array.Sort(info);
-
-                int result = Vote(info, trainingLabels, 3, k);
-                predictedLabels.Add(result);
-            }
-
-            return predictedLabels;
-        }
-
-        public double CalculateAccuracy(List<int> predictedLabels, List<int> actualLabels)
-        {
-            int correctPredictions = 0;
-            for (int i = 0; i < predictedLabels.Count; i++)
-            {
-                if (predictedLabels[i] == actualLabels[i])
-                    correctPredictions++;
-            }
-            double accuracy = (double)correctPredictions / predictedLabels.Count * 100;
-            return accuracy;
-        }
-    }
-
-    public class IndexAndDistance : IComparable<IndexAndDistance>
-    {
-        public int idx;
-        public double dist;
-
-        public int CompareTo(IndexAndDistance other)
-        {
-            return dist.CompareTo(other.dist);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -118,16 +14,16 @@ namespace KNNImplementation
             string jsonFilePath = "C:\\Users\\Lenovo\\Documents\\GitHub\\Global_Variables\\Myproject\\KNN Investigation and Demo\\KNN new\\KNN\\KNN\\Dataset_KNN.json";
             string jsonData = File.ReadAllText(jsonFilePath);
 
-            // Deserialize JSON data into Dataset object
-            Dataset dataset = JsonConvert.DeserializeObject<Dataset>(jsonData);
 
+            // Deserialize JSON data into Dataset object
+            List<SequenceDataEntry> dataset = JsonConvert.DeserializeObject<List<SequenceDataEntry>>(jsonData);
             // Separate the data into training and testing sets
             List<List<double>> trainingFeatures = new List<List<double>>();
             List<int> trainingLabels = new List<int>();
             List<List<double>> testingFeatures = new List<List<double>>();
             List<int> testingLabels = new List<int>();
 
-            foreach (var entry in dataset.Data)
+            foreach (var entry in dataset)
             {
                 foreach (var sequenceData in entry.SequenceData)
                 {
@@ -183,6 +79,10 @@ namespace KNNImplementation
 
             // Test the classifier with testing data
             List<int> predictedLabels = knnClassifier.Test(testingFeatures, trainingFeatures, trainingLabels, 3);
+            foreach (var label in predictedLabels)
+            {
+                Console.WriteLine(label);
+            }
 
             // Calculate and print accuracy
             double accuracy = knnClassifier.CalculateAccuracy(predictedLabels, testingLabels);
